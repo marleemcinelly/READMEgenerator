287ba58005ee12a1ade2812018b768c70b1fd7b2
Marlee McInelly <marleemcinelly@gmail.com>
2021-01-18 12:58:28 -0700

    added a data array to store user responses

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 // array of questions for user
 const questions = [
 
-    License badge: (here it goes)
-    Description: `What is the title of your project?`
-    Table of Contents: (links to other sections)
-    Instalation: `How is your project installed?`
-    Usage: `How can people use your project?` (list format)
-    Contributing:`What are the contribution guidelines for this project?`
-    Tests: `How is your project tested?`
-    License: `What license doe syour project use?` (list format)
-    Questions: `What is your GitHub username?`,`What is your email address?`,`Are there any additional insructions needed to contact you?`
+    Title: `What is the title of your project?`, //stored in variable 'fileName', then has + ".md" to create the file name?
+    Description: `What does your project do?`,
+    Installation: `How is your project installed?`,
+    Usage: `How can people use your project?` (list format),
+    Contributing:`What are the contribution guidelines for this project?`,
+    Tests: `How is your project tested?`,
+    License: `What license doe syour project use?` (list format),
+    GitHub username: `What is your GitHub username?`,
+    Email address: `What is your email address?`,
+    Question instructions: `Are there any additional insructions needed to contact you?`
 
 ];
+
+const data = {
+    badge: "*****",
+    title: "README",
+    toc: [//figure out how to put links in a README, I assume this is an array of them],
+    description: "A README generator that runs from the command line",
+    install: "This project can be donwloaded from github and run in any coding program. It has a dependency in inquirer.",
+    use: "The project is free to use, with appropriate credit and links to the original github repository",
+    contribute: "Anyone is allowed to contribute. Please add a list of your contributions to the README, along with contact information",
+    test: "run it and see if it breaks",
+    userGitHub: "marleemcinelly",
+    userEmail: "marleemcinelly@gmail.com",
+    qOrC: "*****"
+}
 
 // function to write README file
 function writeToFile(fileName, data) {
