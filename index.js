648f741d57b1e14df10e240f45a67a29e852ac1d
Marlee McInelly <marleemcinelly@gmail.com>
2021-01-18 12:34:37 -0700

    code framework for required prompts

---
+++ index.js
@@ -0,0 +1,26 @@
+// array of questions for user
+const questions = [
+
+    License badge: (here it goes)
+    Description: `What is the title of your project?`
+    Table of Contents: (links to other sections)
+    Instalation: `How is your project installed?`
+    Usage: `How can people use your project?` (list format)
+    Contributing:`What are the contribution guidelines for this project?`
+    Tests: `How is your project tested?`
+    License: `What license doe syour project use?` (list format)
+    Questions: `What is your GitHub username?`,`What is your email address?`,`Are there any additional insructions needed to contact you?`
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
